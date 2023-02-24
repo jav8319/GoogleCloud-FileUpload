381bf0d81c5cb5f763c806e89e99fadd3f52cd65
javier <jav8319@hotmail.com>
2023-02-24 14:18:17 -0500

    config better explained

--- server/config/connectionGC.js
+++ server/config/connectionGC.js
@@ -1,14 +1,14 @@
 import { Storage } from '@google-cloud/storage';
 
 
-const path ='./config/react-ecommerce-377703-ba30d4c2432a.json'
+const path ='./config/myjsonfile.json'
 
 
 
 export const storage = new Storage({
-  projectId: 'react-ecommerce-377703',
+  projectId: 'myprojectId',
   keyFilename: path
 });
 
-export const bucketName = 'ecommerceproducts';
+export const bucketName = 'mybucketname';
 
--- server/config/connectionmdb.js
+++ server/config/connectionmdb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 
-mongoose.connect(process.env.MONGODB_URIE || 'mongodb://127.0.0.1:27017/dataPhotos', {
+mongoose.connect(process.env.MONGODB_URIE || 'mongodb://127.0.0.1:27017/mongoDBdatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 
